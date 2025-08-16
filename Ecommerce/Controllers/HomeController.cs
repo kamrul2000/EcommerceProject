fc344e5ef8b5c085582f659989c35f5724938a7d
Kamrul <Kamrul@DESKTOP-F4R2VH7>
2025-08-16 13:08:19 +0600

    Catagory Added

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -17,14 +17,43 @@ namespace Ecommerce.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string category)
         {
-            var products = await _context.Products.ToListAsync();
-            return View(products);
+            var products = _context.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                if (category == "Popular")
+                    products = products.Where(p => p.Category == "Popular");
+                else if (category == "New")
+                    products = products.Where(p => p.Category == "New");
+            }
+
+            return View(await products.ToListAsync());
         }
+
         public IActionResult Privacy()
         {
             return View(Privacy);
         }
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Get related products from the same category, excluding current product
+            var related = _context.Products
+                .Where(p => p.Category == product.Category && p.Id != product.Id)
+                .Take(4) // limit to 4 items
+                .ToList();
+
+            ViewBag.RelatedProducts = related;
+
+            return View(product);
+        }
+
     }
 }
